7eee4517bd5a0da390bb3a6b2a0a97295cff20cb
hafed <hafed.zouyed@univ-constantine2.dz>
2024-10-08 19:17:03 +0100

    tp1 exo3

---
+++ tp1/exo3.dart
@@ -0,0 +1,13 @@
+bool palindrome(String word) {
+  for (int i = 0; i < word.length / 2; i++) {
+    if (word[i] != word[word.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void main() {
+  print(palindrome('abba')); // true
+  print(palindrome('abcdefg')); // false
+}
